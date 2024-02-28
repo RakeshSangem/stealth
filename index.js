c2161933906419d911acd883d505d228e5e6f9c7
zelfroster <sumitkumarsoni123@gmail.com>
2024-02-28 21:57:59 +0530

    feat: express app setup

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+import dotenv from "dotenv";
+import express from "express";
 
+// Setup .env
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.json({ msg: "Stealth startup nodemon" });
+});
+
+app.get("/stealth", (_, res) => {
+  res.json({ msg: "Hidden secret message" });
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on http://localhost:8080");
+});
